b948fdaeaef149ef9155d8827f7d71c17a0651cb
abner.jacobsen@gmail.com <abner.jacobsen@gmail.com@027bb325-fc36-ad86-cf35-cd2980497417>
2011-01-12 22:25:20 +0000

    Added portuguese-br translation to module zendesksso - user interface

---
+++ trunk/Other/Zendesk/Zendesksso/user/portuguese-br.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['zendesksso']['unablegetuserid'] = 'Impossível obter seu ID de usuário';
+$lang['zendesksso']['mustbelogged'] = 'Você não está autorizado a acessar o Help Desk';
+$lang['zendesksso']['clicktologin'] = 'Clique aqui para logar no Help Desk';
+
+?>
